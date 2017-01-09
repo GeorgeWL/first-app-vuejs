e4f9a5f2734c4d00644177eea01ad6450b0eaf24
LAPTOP-SG44TT74\george <georgelangham1990@gmail.com>
2017-01-09 22:00:15 +0000

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-first-app-vuejs
+##first-app-vuejs
+
+Due to the app needing reddit API calls, it will not work unless on a server
+
+Place onto a server, or run locally using MAMP or Python to view the app fully.
+
+I'm currently running my version from my portfolio site, [here](http://projects.georgelangham.co.uk/vue/index.html)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,45 @@
-/* -----------------
-   Initialize app
------------------*/
+//parent | sub containing list of posts
+var subreddit = Vue.component('subreddit', {
+    template: '#subreddit',
+    props: ['name'],
+    // create a component using the template in the index
+    data: function() {
+        return {
+            posts: []
+        }
+
+    },
+
+    created: function() {
+        this.$http.get("htps://www.reddit.com/r" + this.name + "/top.json?limit=5")
+            // for name defined in named sub, call a GET request of the top 5 posts in json format.
+            .then(function(resp) {
+                this.posts = resp.data.data.children;
+                // populate children from collected data
+            });
+    }
+});
+// custom filtering
+Vue.filter('truncate', function(value) {
+    var length = 55;
+    if (value.length <= length) {
+        return value;
+    } else {
+        return value.substring(0, length) + '...';
+    }
+});
+// Checks length of each value,
+// if longer than 55, only shows up to character 55,
+// then follows with...
+
+Vue.filter('uppercase', function(value) {
+    return value.toUpperCase();
+});
+//return the value after converting to uppercase
+    /* -----------------
+       Initialize app
+    -----------------*/
 
 new Vue({
-  el: '#main'
+    el: '#main'
 })
